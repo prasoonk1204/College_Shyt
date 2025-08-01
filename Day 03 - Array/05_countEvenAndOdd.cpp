4f697bfa0c8c728a740aa201dcce6950ccfa2330
Prantor-Das <prantor.college@gmail.com>
2025-08-01 10:47:05 +0530

    Count Even and Odd Numbers in an Array

--- Day 03 - Array/05_countEvenAndOdd.cpp
+++ Day 03 - Array/05_countEvenAndOdd.cpp
@@ -1,1 +1,38 @@
 // 5. Count Even and Odd Numbers in an Array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    
+    int arr[n];
+    cout << "Enter elements: ";
+    
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    int evenCount = 0, oddCount = 0;
+
+    for(int i = 0; i < n; i++) {
+        if(arr[i] % 2 == 0)
+            evenCount++;
+        else
+            oddCount++;
+    }
+
+    cout << "Number of even elements: " << evenCount << endl;
+    cout << "Number of odd elements: " << oddCount << endl;
+
+    return 0;
+}
+
+// Output
+
+// Enter the number of elements: 7
+// Enter elements: 10 11 32 33 45 68 55
+// Number of even elements: 3
+// Number of odd elements: 4
