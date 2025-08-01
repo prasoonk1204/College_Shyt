b2e9948241c5202cd613f9e2887db8f24c1edb43
prasoonk1204 <prasoonkumar467@gmail.com>
2025-08-01 10:19:21 +0530

    01_maxElem added

--- Day 03 - Array/01_maxElem.cpp
+++ Day 03 - Array/01_maxElem.cpp
@@ -1,1 +1,28 @@
 // 1. Find the Maximum Element in an Array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    
+    int arr[n];
+    cout << "Enter elements: ";
+    
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    
+    int max=arr[0];
+    for (int j=1; j<n; j++){
+        if(max<arr[j]){
+            max=arr[j];
+        }
+    }
+    
+    cout << max << " is the largest number.";
+    
+    return 0;
+}
