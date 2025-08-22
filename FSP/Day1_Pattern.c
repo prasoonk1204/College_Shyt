a14b14015c322aba86a7503ae4dc44b459eac057
PRANTOR DAS <prantor.college@gmail.com>
2025-08-22 16:44:33 +0530

    Update Day1_Pattern.c

--- FSP/Day1_Pattern.c
+++ FSP/Day1_Pattern.c
@@ -62,5 +62,18 @@ int main() {
         printf("\n");
     }
 
+    //////////////////////////////////////////
+    printf("\n");
+    
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (i==1 || j==1 || i==n || j==n)
+                printf("* ");
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+
     return 0;
 }
