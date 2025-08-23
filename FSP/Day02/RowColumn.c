86a0554f2940885a9afb9b1ada6568416a3f4d35
Prantor-Das <prantor.college@gmail.com>
2025-08-23 10:51:47 +0530

    row column pattern

---
+++ FSP/Day02/RowColumn.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int i,j,n;
+    printf("Enter no. of rows and columns: ");
+    scanf("%d",&n);
+    
+    int a[n][n];
+    
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            if(i==j && i+j==n-1){
+                a[i][j] = 30;
+            } else if(i==j){
+               a[i][j] =10; 
+            } else if(i+j==n-1) {
+                a[i][j] = 20;
+            } else {
+                a[i][j]=0;
+            }
+        }
+    }
+    
+    printf("Output: \n");
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            printf("%4d ",a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
