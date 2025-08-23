120e656b0ee393f13f4efa40c8728f88da0db5eb
Prantor-Das <prantor.college@gmail.com>
2025-08-23 14:50:03 +0530

    pattern using recursion

---
+++ FSP/Day02/question.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void foo(int n, int sum)
+ { 
+ int k = 0, j = 0; 
+ if (n == 0) return; 
+ k = n % 10; 
+ j = n / 10; 
+ sum = sum + k; 
+ foo (j, sum);
+ printf ("%d \n", sum); 
+ printf ("%d ", k);
+} 
+int main () 
+{ 
+int a = 2048, sum = 0;
+foo (a, sum); 
+	printf ("%d\n", sum); 
+}
+
+// Output
+// 14 
+// 2 12 
+// 0 12 
+// 4 8 
+// 8 0
