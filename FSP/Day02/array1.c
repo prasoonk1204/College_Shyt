3dd452e9d82aa9d9f03808ffe2b8f3575294c6ed
Prantor-Das <prantor.college@gmail.com>
2025-08-23 10:25:04 +0530

    refactor structure and 2D array problem

---
+++ FSP/Day02/array1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int i,j,n,k=1;
+    printf("Enter no. of rows and columns: ");
+    scanf("%d",&n);
+    
+    int a[n][n];
+    
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            a[i][j]=k;
+            k++;
+        }
+    }
+    
+    printf("Output: \n");
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            printf("%4d ",a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
