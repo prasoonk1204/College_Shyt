50dc2913e3d3a5ca3a4dcfc163772694173d252a
Prantor-Das <prantor.college@gmail.com>
2025-09-06 15:36:57 +0530

    adding input to p3

--- JavaFSP/Day1/P3.java
+++ JavaFSP/Day1/P3.java
@@ -1,10 +1,13 @@
 // WAP in java to take two numbers and find their sum and display the result using the concept of class and object.
 
+import java.util.*;
 class Sum{
     int a,b,sum;
     void input(){
-        a = 10;
-        b = 20;
+        System.out.println("Enter two inputs: ");
+        Scanner sc = new Scanner(System.in);
+        a =sc.nextInt();
+        b = sc.nextInt();
     }
     void calc(){
         sum=(a+b);
