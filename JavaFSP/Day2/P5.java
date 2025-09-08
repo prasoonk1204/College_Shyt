6b3690d55a805ae2e8aa8b145c013fb1c66f3df2
Prantor-Das <prantor.college@gmail.com>
2025-09-08 11:27:11 +0530

    WAP in java to find the sum of 2 nos. where the nos. are taken by command line

---
+++ JavaFSP/Day2/P5.java
@@ -0,0 +1,13 @@
+// WAP in java to find the sum of 2 nos. where the nos. are taken by command line
+class P5 {
+    public static void main(String a[]) {
+        int x = Integer.parseInt(a[0]);
+        int y = Integer.parseInt(a[1]);
+        int z = x+y;
+        System.out.println("The sum is "+z);
+    }
+}
+
+// javac P5.java
+// java P5 10 20
+// The sum is 30
