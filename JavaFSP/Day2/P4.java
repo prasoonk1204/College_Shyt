1e2eb8501a38f8b6d375743bdd251579fd17ab54
Prantor-Das <prantor.college@gmail.com>
2025-09-08 11:14:15 +0530

    WAP in java to display command line argument.

---
+++ JavaFSP/Day2/P4.java
@@ -0,0 +1,9 @@
+// WAP in java to display command line argument.
+
+class P4 {
+    public static void main(String a[]) {
+        for(int i=0;i<a.length;i++){
+            System.err.println(a[i]);
+        }
+    }
+}
