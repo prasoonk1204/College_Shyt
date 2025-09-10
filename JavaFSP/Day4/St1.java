cd888b4fb8951afaabd81bfc66c639fcdd6a4803
Prantor-Das <prantor.college@gmail.com>
2025-09-10 16:33:52 +0530

    string

---
+++ JavaFSP/Day4/St1.java
@@ -0,0 +1,7 @@
+public class St1 {
+    public static void main(String[] args) {
+        String str = "Hello how r u";
+        int len = str.length();
+        System.out.println("The string length is: " + len );
+    }
+}
