8eabfbd9d134d176fd06ed3e3983084808e2a483
Prantor-Das <prantor.college@gmail.com>
2025-10-11 21:12:20 +0530

    Write a program in C++ to show an example of unary operator

---
+++ C++_College/Day_08_Operator_Overloading/01.cpp
@@ -0,0 +1,10 @@
+// Write a program in C++ to show an example of unary operator.
+
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 5;
+    cout << "The value of a is " << a << endl;
+    cout << "The value of a is " << -a << endl;
+    return 0;
+}
